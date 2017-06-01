c439b262540b997df1c83a41f19c39d7b0d9d1fd
kor-coeo <kor.coeo@gmail.com>
2017-06-01 07:51:11 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'About')
+
+@section('content')
+  <h1>About</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'Help')
+
+@section('content')
+  <h1>Help</h1>
+@stop
