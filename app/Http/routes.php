7227d4fabc6fd7e5111bd83295cca6e9243aa2a1
kor-coeo <kor.coeo@gmail.com>
2017-06-02 11:17:35 +0000

    Add password resets & email configuration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,3 +23,8 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+Route::get('password/email', 'Auth\PasswordController@getEmail')->name('password.reset');
+Route::post('password/email', 'Auth\PasswordController@postEmail')->name('password.reset');
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset')->name('password.edit');
+Route::post('password/reset', 'Auth\PasswordController@postReset')->name('password.update');
