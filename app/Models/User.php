ead787470bb0d8bb719869f666c872daca9ba88c
kor-coeo <kor.coeo@gmail.com>
2017-06-02 15:22:11 +0000

    Mod User fix Auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,8 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
+use Auth;
+
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
